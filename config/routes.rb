b8ece38613deec2697352f92c5d0da80d11825b3
luckyligovi <lucky.luvihi@student.moringaschool.com>
2022-12-04 16:52:44 +0300

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
+# Rails.application.routes.draw do
+  # resources :plants
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/plants", to: "plants#index"
+  # get "/plants", to: "plants#index"
+  # get "/students", to: "students#index"
+
+  resources :plants, only:[:index, :show, :create]
+
+  # get "/students/highest-grade", to: "students#highest_grade"
+
 end
